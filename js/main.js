c5f9609161fb569d08c7c774aef7896e5cc502dd
Hurlyburly2 <dougdougmann@gmail.com>
2019-07-23 15:57:12 -0400

    graph area and margins created and placed

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
+let margin = { top: 10, right: 10, bottom: 200, left: 100 }
 
+let canvasWidth = 600 - margin.right - margin.left;
+let canvasHeight = 500 - margin.top - margin.bottom;
+
+let svg = d3.select("#chart-area").append("svg")
+  .attr("width", canvasWidth + margin.right + margin.left)
+  .attr("height", canvasHeight + margin.top + margin.bottom)
+  
+let graphGroup = svg.append("g")
+  .attr("transform", "translate(" + margin.left + ", " + margin.top + ")")
 
 d3.json("data/revenues.json").then(data => {
   
@@ -6,5 +16,12 @@ d3.json("data/revenues.json").then(data => {
     month.revenue = parseInt(month.revenue)
     month.profit = parseInt(month.profit)
   })
+  
+  graphGroup.append("rect")
+    .attr("x", 0)
+    .attr("y", 0)
+    .attr("width", canvasWidth)
+    .attr("height", canvasHeight)
+    .attr("fill", "grey")
   
 })
